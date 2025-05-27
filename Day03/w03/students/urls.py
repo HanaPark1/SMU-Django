f0b50aae07dc336d69847b5e0b5c7978f83f9e33
HanaPark1 <ydjxlst@gmail.com>
2025-05-27 17:30:09 +0900

    study: 데이터 전송 방식

--- Day03/w03/students/urls.py
+++ Day03/w03/students/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'students'
 urlpatterns = [
-    path('list/', views.list, name="list")
+    path('list/', views.list, name="list"),
+    path('view/', views.view, name="view"),
+    path('write/', views.write, name='write'),
+    path('result/', views.result, name='result'),
+    path('send/<str:name>/<int:age>/', views.send, name='send'),
 ]
--- Day03/w03/students/views.py
+++ Day03/w03/students/views.py
@@ -10,3 +10,26 @@ def list(request):
     hobbys = request.POST.getlist("hobby")
     context = {"id":id, "pw":pw, "gender":gender,'tel':tel,'hobby':hobbys}
     return render(request, 'list.html', context)
+
+def view(request):
+    name = request.GET.get('name')
+    age = request.GET.get('age')
+    context = {'name':name, 'age':age}
+    return render(request,'view.html', context)
+
+def write(request):
+    return render(request,'write.html')
+
+def result(request):
+    name = request.POST.get('name')
+    kor = int(request.POST.get('kor'))
+    eng = int(request.POST.get('eng'))
+    total = kor+eng
+    hobbys = request.POST.getlist('hobby')
+    print(name,kor,total)
+    context = {'name':name,'kor':kor,'eng':eng,'total':total,'hobby':hobbys}
+    return render(request,'result.html',context)
+
+def send(request,name,age):
+    context = {'name':name, 'age':age}
+    return render(request, 'send.html',context)
