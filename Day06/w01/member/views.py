66d03e7ea74f043bb26912e4b9150b889db98c57
HanaPark1 <ydjxlst@gmail.com>
2025-05-30 12:23:37 +0900

    study: 로그인/로그아웃 세션 할당 및 삭제

--- Day06/w01/member/views.py
+++ Day06/w01/member/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from member.models import Member
 
 # Create your views here.
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "GET" :
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        id = request.POST.get('id')
+        pw = request.POST.get('pw')
+        try :
+            qs = Member.objects.get(id=id)
+            if qs.pw == pw:
+                # 세션 할당
+                request.session['session_id'] = id
+                txt = 1
+            else:
+                txt = -1 # 아이디 존재, 비번 오류
+        except:
+            txt = 0 # 아이디가 없음
+            
+        # try :
+        #     qs = Member.objects.get(id=id, pw=pw)
+        #     # 세션 할당
+        #     request.session['session_id'] = id
+        #     txt = 1
+        # except:
+        #     txt = 0
+        context = {'msg':txt}
+        return render(request, 'login.html', context)
+
+def logout(request):
+    # 세션 삭제
+    # request.session.clear() #모두삭제
+    # del request.session['session_id'] # 세션 하나 삭제
+    del request.session['session_id']
+    return redirect('/')
