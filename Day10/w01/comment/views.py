3124b6b3c629392f1f63a94161d4408d89272894
HanaPark1 <ydjxlst@gmail.com>
2025-06-10 18:24:29 +0900

    study: 하단 댓글 작성

---
+++ Day10/w01/comment/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from django.core import serializers #json type
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+
+def list(request):
+    return HttpResponse('데이터 전달')
