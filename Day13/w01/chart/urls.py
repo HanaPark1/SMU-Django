c374852534d288e1ebad5255a9776a652e32924f
HanaPark1 <ydjxlst@gmail.com>
2025-06-18 09:30:54 +0900

    study: ajax 활용 차트 그리기

--- Day13/w01/chart/urls.py
+++ Day13/w01/chart/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name='chart/'
 urlpatterns = [
     path('chlist/', views.chlist, name='chlist'),
-   
+    path('chajax/', views.chajax, name='chajax'),
+    path('chlist2/', views.chlist2, name='chlist2'),
+    path('chlist3/', views.chlist3, name='chlist3'),
+    path('chajax2/', views.chajax2, name='chajax2'),
+
 ]
