aaf81bab5eea53220c1f1b36db4665838f776f8b
HanaPark1 <ydjxlst@gmail.com>
2025-06-23 09:59:54 +0900

    study: 글쓰기

--- shopProject/shop/customer/urls.py
+++ shopProject/shop/customer/urls.py
@@ -1,6 +1,8 @@
 
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'customer'
 urlpatterns = [
@@ -8,3 +10,6 @@ urlpatterns = [
     path('view/<int:bno>/', views.view, name='view'),
     path('write/', views.write, name='write'),
 ]
+
+# 파일업로드시 url구성 , urlpatterns 에 추가로 설정이 들어감.
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shopProject/shop/customer/views.py
+++ shopProject/shop/customer/views.py
@@ -1,32 +1,66 @@
-from django.shortcuts import render
-from customer.models import Customer
+from django.shortcuts import render,redirect
 from django.core.paginator import Paginator
+from customer.models import Customer
+from member.models import Member
 
-# Create your views here.
-
-def list(request):
-    #요청하는 page 번호 가져오기 str->int
-    page = int(request.GET.get('page',1))
+# 게시글 상세보기
+def view(request,bno):
+    qs = Customer.objects.get(bno=bno)
     
-    qs = Customer.objects.all().order_by('-ntchk', '-bgroup', 'bstep')
-    # 10개 단위로 qs 분리
+    qs.bhit += 1
+    qs.save()
+    context = {'customer':qs}
+    response = render (request,'customer/view.html',context)
+    
+    # 쿠키 확인
+    if request.COOKIES.get('cookie_name') is not None:
+        ## 조회수 방지 쿠키가 있을때
+        cookies = request.COOKIES.get('cookie_name')
+    else:
+        ## 조회수 방지 쿠키가 없을때
+        response.set_cookie('cookie_name', bno)
+        
+    
+    return response
+
+# 게시글 쓰기 - get:글쓰기페이지, post:글쓰기저장
+def write(request):
+    if request.method == 'GET':
+        return render(request,'customer/write.html')
+    elif request.method == 'POST':
+        btitle = request.POST.get('btitle')
+        ## session 서버 
+        id = request.session['session_id']
+        member = Member.objects.get(id=id)
+        bcontent = request.POST.get('bcontent')
+        bfile1 = request.FILES.get('bfile1','')
+        bfile2 = request.FILES.get('bfile2','')
+        qs = Customer.objects.create(btitle=btitle,member=member,bcontent=bcontent,bfile1=bfile1,bfile2=bfile2)
+        qs.bgroup = qs.bno
+        qs.save()
+        print('------------------')
+        print(btitle,id,bcontent,bfile1,bfile2)
+        print('------------------')
+        context = {'msg':1}
+        return render(request,'customer/write.html',context)
+        
+
+# 게시판리스트
+def list(request):
+    # 요청하는 page번호 가져오기, str타입 -> int타입
+    page = int(request.GET.get('page',1))
+    # db에서 데이터 가져오기
+    qs = Customer.objects.all().order_by('-ntchk','-bgroup','bstep')
+    # 10개단위로 qs로 분리시킴
     paginator = Paginator(qs,10)
     
     # 가져올 페이지 선택
     customerList = paginator.get_page(page)
-    print('-------------')
+    print('-----------------')
     print(customerList)
-    print('-------------')
+    print('-----------------')
     
-    # 게시글 10개, 현재 페이지 보냄
-    context = {'list':customerList, 'page':page}
-    return render(request, 'customer/list.html', context)
-
-def view(request, bno):
-    return render(request, 'customer/view.html')
-
-def write(request):
-    if request.method == 'GET':
-        return render(request, 'customer/write.html')
-    elif request.method == 'POST':
-        return render(request, 'customer/write.html')
+    # 게시글 10개, 현재페이지 보냄
+    context = {'list':customerList,'page':page}
+    return render(request,'customer/list.html',context)
+    # return redirect('/customer/list/')
